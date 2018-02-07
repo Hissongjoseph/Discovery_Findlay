3f0caf8a57c9a9230bfce8c60fe0335c5c5e78d7
Joseph Hissong <hissong.joseph@gmail.com>
2018-02-07 15:57:37 -0500

    first commit

---
+++ Schedule_Activity.java
@@ -0,0 +1,13 @@
+package com.discovery.hisso.discovery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Schedule_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_);
+    }
+}
